c21b9e00c3afb8b13754bbce8a6cc3687a9f7507
Omar Ahmed Rouby <164004472+omaRouby@users.noreply.github.com>
2024-06-13 15:52:46 +0300

    Update create_backend_resources.tf

--- terraform/create_backend_resources.tf
+++ terraform/create_backend_resources.tf
@@ -1,29 +1,29 @@
 
-# resource "aws_s3_bucket" "terraform_state" {
-#   bucket = "ivolve-state-bucket"
-#   acl    = "private"
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "ivolve-state-bucket"
+  acl    = "private"
 
-#   versioning {
-#     enabled = true
-#   }
+  versioning {
+    enabled = true
+  }
 
-#   server_side_encryption_configuration {
-#     rule {
-#       apply_server_side_encryption_by_default {
-#         sse_algorithm = "AES256"
-#       }
-#     }
-#   }
-# }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
 
-# resource "aws_dynamodb_table" "terraform_lock" {
-#   name         = "terraform-lock"
-#   billing_mode = "PAY_PER_REQUEST"
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
 
-#   hash_key = "LockID"
+  hash_key = "LockID"
 
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
