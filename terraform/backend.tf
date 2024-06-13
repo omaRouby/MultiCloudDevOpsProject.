b205059f9dee2fce95878c429243ca942c0de073
Omar Ahmed Rouby <164004472+omaRouby@users.noreply.github.com>
2024-06-13 15:53:07 +0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "your-terraform-state-bucket"
-#     key            = "terraform/state/terraform.tfstate"
-#     region         = "us-east-1"
-#     encrypt        = true
-#     dynamodb_table = "terraform-lock"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "ivolve-state-bucket"
+    key            = "terraform/state/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
